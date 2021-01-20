e888c3352c35b1b00e060699e3f08e363ecaafea
peret4s <77710869+peret4s@users.noreply.github.com>
2021-01-20 13:31:29 +0700

    Create Tools.sh

---
+++ Tools.sh
@@ -0,0 +1,149 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay BY QILLA
+echo
+sleep 2
+echo $red"__________________________________________________"
+echo
+echo $red"Author : raqilla arta"
+echo
+echo $red"Youtube : riswadi arta"
+echo
+echo $red"__________________________________________________"
+echo
+echo $red"selamat datang di tools ini"
+echo
+sleep 2
+echo $yellow"silahkan pilih tools nya :"
+echo
+echo $blue"1.)install bahan dulu bagi yang belum"
+echo
+echo $purple"2.)stabilkan jaringan"
+echo
+echo $red"3.)hack fb id:Termux password:omaliptv"
+echo
+echo $yellow"4.)deface + script deface"
+echo
+echo $cyan"5.)spam tools"
+echo
+echo $blue"6.)nuyul aplikasi lengkap"
+echo
+echo $yellow"7.)ubah tampilan termux keren"
+echo
+echo $cyan"8.)buat web phising weeman facebook"
+echo
+echo $yellow"9.)buat akses login"
+echo
+echo $red"0.)mau keluar lewat sini aja ya :)"
+echo
+echo $yellow
+read -p "pilih mana woy :" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+sleep 2
+pkg install python2
+pkg install git
+pkg install bash
+pkg install nano
+pkg install figlet
+pkg install toilet
+pip2 install requests
+pip2 install bs4
+pip2 install mechanize
+cd /sdcard
+termux-setup-storage
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f big -F gay QILLA
+echo $green"stabilkan jaringan by QILLA"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+sleep 2
+git clone https://github.com/storiku/darkfbv1.9
+cd darkfbv1.9
+python2 dark.pyc
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+sleep 2
+git clone https://github.com/404rgr/webdav77
+cd webdav77
+bash install.sh
+bash webdav77.sh
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+sleep 2
+git clone https://github.com/Zidan-ID17/Spam-Tools
+cd Spam-Tools
+python2 umbrella.py
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+sleep 2
+git clone https://github.com/MaulanaRyM/Nuyul-Apk
+cd Nuyul-Apk
+bash bot.sh
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+sleep 2
+https://github.com/4NK3R-PRODUCT1ON/T4MPILAN-V5
+cd T4MPILAN-V5
+python2 style5.py
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+git clone https://github.com/samyoyo/weeman
+cd weeman
+python2 weeman.py
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+clear
+git clone https://github.com/Ublimjo/Termux-login
+cd Termux-login
+bash setup.sh
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $yellow
+figlet "BYE"
+echo $green"terimakasih sudah menggunakan tools ini"
+echo $green"jangan lupa kembali lagi"
+echo $green"dan subscribe channel riswadi arta"
+sleep 3
+exit
+fi
